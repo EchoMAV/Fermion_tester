df332a5902b012cc58b595d9f1409e99b97abfa4
Brad Stinson <bradstinson@gmail.com>
2025-01-05 15:22:16 -0500

    Update test_eo.sh

--- test_eo.sh
+++ test_eo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-pistreamer --config_file=/usr/lib/python3.11/dist-packages/pistreamer/477-Pi4.json --radio_type="test" --gcs_ip=10.0.0.63
+TARGET_IP=10.0.0.63
+
+pistreamer --config_file=/usr/lib/python3.11/dist-packages/pistreamer/477-Pi4.json --radio_type="test" --gcs_ip=${TARGET_IP}
