38ea602c667ed750de066a37492286727bc3d810
Benjamin <178317378+benjamincastor@users.noreply.github.com>
2025-05-08 14:49:15 -0400

    Update test_eo.sh

--- test_eo.sh
+++ test_eo.sh
@@ -3,4 +3,4 @@
 TARGET_IP=10.0.0.38
 SUDO=$(test ${EUID} -ne 0 && which sudo)
 $SUDO systemctl stop video-eo
-pistreamer --config_file=/usr/lib/python3.11/dist-packages/pistreamer/477-Pi4.json --radio_type="herelink" --gcs_ip=${TARGET_IP} --verbose
+pistreamer --config_file=/usr/lib/python3.11/dist-packages/pistreamer/477-Pi4.json --radio_type="test" --gcs_ip=${TARGET_IP} --verbose
